b8eceda7be4560aeb190f2ede1c89eb2d1af7f93
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-09-24 10:05:14 +0100

    feat(home): add Navbar component to Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import Hero from "../components/Hero";
-import About from "../components/About";
 import ServiceInfo from "../components/ServiceInfo";
 import TravelNote from "../components/TravelNote";
 import Gallery from "../components/Gallery";
 import Guestbook from "../components/Guestbook";
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <div>
       <Hero />
+      <Navbar />
       <ServiceInfo />
       <TravelNote />
     </div>
