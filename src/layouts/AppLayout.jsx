aa4640f8980d4c2798ec8deb2acceed068989386
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-09-24 10:05:52 +0100

    feat(applayout): remove Navbar component from AppLayout

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function AppLayout() {
   return (
     <>
-      <Navbar />
       <main>
         <Outlet />
       </main>
